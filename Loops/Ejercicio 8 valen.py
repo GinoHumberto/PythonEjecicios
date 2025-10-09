7a1df3e5ccd840ae95d389d65277020359174ec7
GinoHumberto <deldealavista@gmail.com>
2025-10-09 09:25:33 -0300

    Ejercicio de transponer matrices pero realmente transponiendo

--- Loops/Ejercicio 8 valen.py
+++ Loops/Ejercicio 8 valen.py
@@ -8,15 +8,32 @@
 numeros = [[1,2,3],[4,5,6], [7,8,9]]
 
 #Matriz normal
-for i in (numeros):
-    for j in (i):
-        print(j, end = '')
-    print()
-print()
-#Matriz transpuesta 
-iterador = 0
-while len(numeros) != iterador:
-    for i in (numeros):
-        print(i[iterador], end = '')
-    iterador += 1
+#for i in (numeros):
+#    for j in (i):
+#        print(j, end = '')
+#    print()
+#print()
+##Matriz transpuesta 
+#iterador = 0
+#while len(numeros) != iterador:
+#    for i in (numeros):
+#        print(i[iterador], end = '')
+#    iterador += 1
+#    print()
+
+######################################################################
+#       Ejercicio pero ahora realmente transponiendo la matriz       #
+######################################################################
+
+# i = fila
+# j = columna
+
+# El orden en este caso es n x n
+
+matriz = [[1,2,3],[4,5,6], [7,8,9]]
+orden = len(matriz)
+
+for i in range(orden):
+    for j in range(orden):
+        print(matriz[j][i], end = '')
     print()
